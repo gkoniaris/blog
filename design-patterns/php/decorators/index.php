dfdd238c54cc4c4dbb176cd1fc9cdfae2f4429d8
George Koniaris <koniaris.giwrgos@gmail.com>
2020-08-09 13:09:16 +0300

    Change products prices

--- design-patterns/php/decorators/index.php
+++ design-patterns/php/decorators/index.php
@@ -6,14 +6,14 @@ $sandwich = new Product('Sandwich', 'food', 200, 3.5);
 echo ($sandwich->price());
 echo ("\n");
 
-$tomatoes = new ProductToBeDecorated('Tomatoes', 'food', 5000, 3.5);
+$tomatoes = new ProductToBeDecorated('Tomatoes', 'food', 5000, 7.5);
 $tomatoes = new VatDecorator($tomatoes);
-$tomatoes = new ShippingDecorator($tomatoes);
+// $tomatoes = new ShippingDecorator($tomatoes);
 
 echo ($tomatoes->price());
 echo ("\n");
 
-$potatoes = new DecoratorBuilder(new ProductToBeDecorated('Potatoes', 'food', 5000, 3.5));
+$potatoes = new DecoratorBuilder(new ProductToBeDecorated('Potatoes', 'food', 5000, 7.5));
 $potatoes = $potatoes
             ->applyDecorator(VatDecorator::class)
             ->applyDecorator(ShippingDecorator::class);
